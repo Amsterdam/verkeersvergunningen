e3b31d9b35f489d8884699f963b22dded36475f2
Hielke <kramerh@gmail.com>
2021-09-07 15:13:38 +0200

    Added first failing tests

--- tests/test_verkeersvergunningen.py
+++ tests/test_verkeersvergunningen.py
@@ -1,8 +1,18 @@
-import pytest
+import json
+from datetime import date, timedelta
+
 
 class TestVerkeersvergunningen:
-    def test_something(self):
-        assert 1 == 1
+    def setup(self):
+        self.URL = '/zwaarverkeer/'
 
-    def test_something_else(self):
-        assert 1 == 2
+    def test_vanilla(self, client):
+        payload = {'number_plate': '1234AB'}
+
+        response = client.post(self.URL, json.dumps(payload), content_type='application/json')
+
+        assert response.status_code == 200
+        assert json.loads(response.content)['number_plate'] == payload['number_plate']
+        assert json.loads(response.content)['has_permit'] == True
+        # assert json.loads(response.content)['date_from'] == date.today().isoformat()
+        # assert json.loads(response.content)['date_until'] == (date.today() + timedelta(days=1)).isoformat()

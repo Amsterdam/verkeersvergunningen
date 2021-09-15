9e78c2e40fde9c580ea315980d4deadff0738440
Hielke <kramerh@gmail.com>
2021-09-15 11:09:02 +0200

    Added swagger

--- src/main/urls.py
+++ src/main/urls.py
@@ -13,9 +13,26 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, re_path, path
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Verkeersvergunningen API",
+      default_version='v1',
+      description="An API to get the verkeersvergunningen. Currently only for zwaar verkeer.",
+      license=openapi.License(name="Mozilla Public License Version 2.0"),
+   ),
+   public=True,
+   permission_classes=[permissions.AllowAny],
+)
 
 urlpatterns = [
-    path('zwaarverkeer/', include('zwaarverkeer.urls'))
+    path('zwaarverkeer/', include('zwaarverkeer.urls')),
+    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path(r'swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
--- src/zwaarverkeer/views.py
+++ src/zwaarverkeer/views.py
@@ -2,6 +2,7 @@ import logging
 
 from braces.views import CsrfExemptMixin
 from dateutil import parser
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework import serializers
 from rest_framework.response import Response
 from rest_framework.views import APIView
@@ -12,24 +13,51 @@ from zwaarverkeer.tools import ImmediateHttpResponse
 log = logging.getLogger(__name__)
 
 
-class HasPermitSerializer(serializers.Serializer):
+class HasPermitRequestSerializer(serializers.Serializer):
     number_plate = serializers.CharField(min_length=6, max_length=6)
     passage_at = serializers.DateTimeField()
+
+
+class HasPermitResponseSerializer(serializers.Serializer):
+    number_plate = serializers.CharField(min_length=6, max_length=6)
+    passage_at = serializers.DateTimeField()
+    has_permit = serializers.BooleanField()
+    permit_type = serializers.CharField(required=False, allow_null=True)
+    date_from = serializers.DateTimeField(required=False, allow_null=True)
+    date_until = serializers.DateTimeField(required=False, allow_null=True)
 
 
 class HasPermitView(CsrfExemptMixin, APIView):
     http_method_names = ['post']
     authentication_classes = [BasicAuthWithKeys]
-    serializer_classes = [HasPermitSerializer]
-    # renderer_classes =
 
+    @swagger_auto_schema(
+        request_body=HasPermitRequestSerializer,
+        responses={200: HasPermitResponseSerializer},  # Define more responses here
+    )
     def post(self, request):
-        number_plate = request.data['number_plate']
+        req_ser = HasPermitRequestSerializer(data=request.data)
+        if not req_ser.is_valid():
+            # RETURN ERRORS CORRECTLY HERE
+            return Response(req_ser.errors)
+
+        number_plate = request.data['number_plate'].upper()
         passage_at = parser.parse(request.data['passage_at'])  # naive local datetime?
 
         try:
             decos = DecosJoin()
-            has_permit = decos.has_permit(number_plate, passage_at)
-            return Response({'number_plate': request.data['number_plate'], 'has_permit': has_permit})
+            has_permit, permit_type = decos.get_permit(number_plate, passage_at)
+            response = {
+                'number_plate': number_plate,
+                'passage_at': request.data['passage_at'],
+                'has_permit': has_permit,
+                'permit_type': permit_type,
+                'date_from': None,
+                'date_until': None,
+            }
+            ser = HasPermitResponseSerializer(data=response)
+            ser.is_valid(raise_exception=True)
+            return Response(ser.data)
+
         except ImmediateHttpResponse as e:
             return e.response

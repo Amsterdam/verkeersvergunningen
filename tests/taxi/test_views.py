ae4b995d6c4053fb7660fa133e3535efd5bd2d69
Nick Zaaijer <n.zaaijer@amsterdam.nl>
2022-09-13 09:48:32 +0200

    test view

--- tests/taxi/test_views.py
+++ tests/taxi/test_views.py
@@ -1,4 +1,6 @@
+from django.http import HttpResponse
 from django.test import TestCase
+import json
 from django.urls import reverse
 from unittest.mock import patch
 from rest_framework import status
@@ -7,22 +9,24 @@ from rest_framework.test import APIRequestFactory
 from taxi.views import PermitView
 
 
-class TestTaxiViews(TestCase):
-    def setUp(self) -> None:
-        pass
+class TestTaxiViews:
 
+    @patch('main.authentication.BasicAuthWithKeys.authenticate',
+           lambda *args, **kwargs: ('user', None))
     @patch('taxi.utils.DecosTaxi.get_decos_key',
            lambda *args, **kwargs: 'bsn_fake_1234567')
     @patch('taxi.utils.DecosTaxi.get_driver_permits',
            lambda *args, **kwargs: ['permit_fake_12345'])
-    def test_get_permits_endpoint(self):
+    def test_post_permits_endpoint(self):
         factory = APIRequestFactory()
         view = PermitView.as_view()
 
-        kwargs = {
-            'bsn': '123456'
-        }
-        url = reverse('taxi_permit', kwargs=kwargs)
-        request = factory.post(content_type="application/json", path=url)
+        data = json.dumps({
+            'bsn': '123456ab'
+        })
+        url = reverse("taxi_exemption")
+        request = factory.post(content_type="application/json",
+                               path=url, data=data,
+                               HTTP_AUTHORIZATION='12345')
         response = view(request)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        assert response.status_code == status.HTTP_200_OK

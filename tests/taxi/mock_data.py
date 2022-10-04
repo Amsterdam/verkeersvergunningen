60e5003cfd73f12655301badc424678fc4111bc0
jjbeekman <jonathanjeroen@gmail.com>
2022-10-04 11:08:28 +0200

    Updated tests

--- tests/taxi/mock_data.py
+++ tests/taxi/mock_data.py
@@ -1,4 +1,7 @@
-def mock_zaaknummer():
+from taxi.enums import PermitParams
+
+
+def mock_driver():
     return {
         "count": 1,
         "content": [
@@ -11,6 +14,41 @@ def mock_zaaknummer():
             }
         ],
     }
+
+
+def mock_handhavingszaken():
+    return {
+        "count": 2,
+        "content": [
+            {
+                "key": "7CAAF40DB75A46BDB5CD5B2A948221B3",
+                "fields": {
+                    "date6": "2022-08-29T00:00:00",
+                    "date7": "2022-09-28T00:00:00",
+                    "dfunction": "Schorsen",
+                },
+            },
+            {
+                "key": "C06C49DC3BAA48E7A38C0D11F97D9770",
+                "fields": {"date6": "2022-09-05T00:00:00", "date7": "2022-09-26T00:00:00"},
+            },
+        ],
+    }
+
+
+def mock_parsed_enforcement_cases():
+    return [
+            {
+                PermitParams.zaakidentificatie.name: "7CAAF40DB75A46BDB5CD5B2A948221B3",
+                PermitParams.geldigVanaf.name: "2022-08-29T00:00:00",
+                PermitParams.geldigTot.name: "2022-09-28T00:00:00",
+            },
+            {
+                PermitParams.zaakidentificatie.name: "C06C49DC3BAA48E7A38C0D11F97D9770",
+                PermitParams.geldigVanaf.name: "2022-09-05T00:00:00",
+                PermitParams.geldigTot.name: "2022-09-26T00:00:00",
+            }
+        ]
 
 
 def mock_permits():
@@ -42,34 +80,6 @@ def mock_permits():
             {
                 "rel": "itemprofile",
                 "href": "https://decosdvl.acc.amsterdam.nl/decosweb/aspx/api/v1/itemprofiles/007992321A8F49E9AF5041EDF1504F20|2E617BAF63B34D3C9606F92E59C9E09F/list",
-            }
-        ],
-    }
-
-
-def mock_handhavingszaken():
-    return {
-        "count": 1,
-        "content": [
-            {
-                "key": "7CAAF40DB75A46BDB5CD5B2A948221B3",
-                "fields": {
-                    "date6": "2022-08-29T00:00:00",
-                    "date7": "2022-09-28T00:00:00",
-                    "dfunction": "Schorsen"
-                },
-                "links": [
-                    {
-                        "rel": "self",
-                        "href": "https://decosdvl.acc.amsterdam.nl/decosweb/aspx/api/v1/items/7CAAF40DB75A46BDB5CD5B2A948221B3",
-                    }
-                ],
-            }
-        ],
-        "links": [
-            {
-                "rel": "itemprofile",
-                "href": "https://decosdvl.acc.amsterdam.nl/decosweb/aspx/api/v1/itemprofiles/007992321A8F49E9AF5041EDF1504F20%7C2E617BAF63B34D3C9606F92E59C9E09F/list",
             }
         ],
     }
--- tests/taxi/test_views.py
+++ tests/taxi/test_views.py
@@ -4,8 +4,6 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.test import APIClient
 import pytest
-from tests.utils import MockResponse
-from .mock_data import mock_handhavingszaken
 from taxi.enums import PermitParams
 
 
@@ -22,7 +20,7 @@ def force_auth(*args, **kwargs):
 class TestUrls:
     @patch("taxi.decos.DecosTaxi._get_driver_decos_key", lambda *args, **kwargs: "bsn_fake_1234567")
     @patch(
-        "taxi.decos.DecosTaxi._get_ontheffingen",
+        "taxi.decos.DecosTaxi._get_ontheffingen_driver",
         lambda *args, **kwargs: [
             {
                 PermitParams.zaakidentificatie.name: "permit_fake_12345",
@@ -54,14 +52,43 @@ class TestUrls:
         )
 
     @patch(
-        "taxi.decos.DecosTaxi._get_ontheffingen",
+        "taxi.decos.DecosTaxi.get_ontheffingen_by_driver_bsn",
         lambda *args, **kwargs: [
             {
-                PermitParams.zaakidentificatie.name: "permit_fake_12345",
-                PermitParams.geldigVanaf.name: "2022-09-05T00:00:00",
-                PermitParams.geldigTot.name: "2022-09-26T00:00:00",
+                "zaakidentificatie": "8E5F8EB000EC4938BC894CA2313E9134",
+                "geldigVanaf": "2022-08-01T00:00:00",
+                "geldigTot": "2025-07-31T00:00:00",
+                "schorsingen": [
+                    {
+                        "zaakidentificatie": "7CAAF40DB75A46BDB5CD5B2A948221B3",
+                        "geldigVanaf": "2022-08-29T00:00:00",
+                        "geldigTot": "2022-09-28T00:00:00",
+                    },
+                    {
+                        "zaakidentificatie": "C06C49DC3BAA48E7A38C0D11F97D9770",
+                        "geldigVanaf": "2022-09-05T00:00:00",
+                        "geldigTot": "2022-09-26T00:00:00",
+                    },
+                ],
             }
         ],
+    )
+    def test_get_ontheffingen_by_driver_bsn(self, client):
+        data = {"bsn": 12345678}
+        url = reverse("taxi_ontheffingen_bsn")
+        response = client.post(url, data=data)
+        assert response.status_code == status.HTTP_200_OK
+        assert PermitParams.zaakidentificatie.name in response.data["ontheffing"][0]
+        assert response.data["ontheffing"][0][PermitParams.zaakidentificatie.name] == "8E5F8EB000EC4938BC894CA2313E9134"
+        assert len(response.data["ontheffing"][0]["schorsingen"]) == 2
+
+    @patch(
+        "taxi.decos.DecosTaxi._get_ontheffing_details",
+        lambda *args, **kwargs: {
+            PermitParams.zaakidentificatie.name: "permit_fake_12345",
+            PermitParams.geldigVanaf.name: "2022-09-05T00:00:00",
+            PermitParams.geldigTot.name: "2022-09-26T00:00:00",
+        },
     )
     @patch(
         "taxi.decos.DecosTaxi._get_handhavingzaken",
@@ -81,10 +108,3 @@ class TestUrls:
         assert PermitParams.zaakidentificatie.name in response.data
         assert response.data[PermitParams.zaakidentificatie.name] == "permit_fake_12345"
         assert response.data["schorsingen"][0][PermitParams.zaakidentificatie.name] == "schorsing_fake_12345"
-
-    @patch("taxi.decos.DecosTaxi._get_response", lambda *args, **kwargs: MockResponse(200, mock_handhavingszaken()))
-    def test_get_ontheffing_detail_endpoint(self, client):
-        kwargs = {"ontheffingsnummer": "123456ab"}
-        url = reverse("taxi_ontheffing_details", kwargs=kwargs)
-        response = client.get(url)
-        assert response.status_code == status.HTTP_200_OK

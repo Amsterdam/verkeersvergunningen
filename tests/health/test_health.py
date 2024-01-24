d501e10f947be61ebe5bab702ca32be6ab317cd3
Jeroen Beekman <j.beekman@amsterdam.nl>
2024-01-24 15:01:59 +0100

    fix test

--- tests/health/test_health.py
+++ tests/health/test_health.py
@@ -11,12 +11,12 @@ def http_client():
 
 class TestViews:
     def test_health_view(self, http_client):
-        response = http_client.get("/status/health")
+        response = http_client.get("/")
         assert response.status_code == 200
         assert response.content == b"Connectivity OK"
 
     @mock.patch("health.views.settings.DEBUG", True)
     def test_debug_false(self, http_client):
-        response = http_client.get("/status/health")
+        response = http_client.get("/")
         assert response.status_code == 500
         assert response.content == b"Debug mode not allowed in production"
